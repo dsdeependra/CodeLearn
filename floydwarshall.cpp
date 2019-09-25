df58379bd22944a6bc1b1fcf227ddea251c8df45
Singh <Deependra.Singh@emc.com>
2019-09-25 10:55:11 +0530

    transitiveClosure

--- floydwarshall.cpp
+++ floydwarshall.cpp
@@ -39,9 +39,15 @@ void floydWarshall(int graph[][V]){
 }
 
 int main(){
-    int graph[V][V] = { {0, 5, INF, 10},  
-                        {INF, 0, 3, INF},  
-                        {INF, INF, 0, 1},  
+    // int graph[V][V] = { {0, 5, INF, 10},  
+    //                     {INF, 0, 3, INF},  
+    //                     {INF, INF, 0, 1},  
+    //                     {INF, INF, INF, 0}  
+    //                 }; 
+
+     int graph[V][V] = { {0, 1, 1, INF},  
+                        {INF, 0, 1, INF},  
+                        {1, INF, 0, 1},  
                         {INF, INF, INF, 0}  
                     };  
   
